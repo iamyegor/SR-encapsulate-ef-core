ff3e43529916cb8b446f951290c03279bb16cbd7
yegor <astery227@gmail.com>
2024-04-19 12:39:04 +0300

    Q: Create a generic repository

--- src/Api/StudentRepository.cs
+++ src/Api/StudentRepository.cs
@@ -14,13 +14,41 @@ public class StudentRepository
         Student student = _context.Students.Find(id);
 
         if (student == null)
-        {
             return null;
-        }
 
         _context.Entry(student).Collection(x => x.Enrollments).Load();
         _context.Entry(student).Collection(x => x.SportsEnrollments).Load();
 
         return student;
+    }
+
+    public void Save(Student student)
+    {
+        // Use one of:
+
+        _context.Students.Add(student);
+        _context.Students.Update(student);
+        _context.Students.Attach(student);
+    }
+}
+
+public class CourseRepository
+{
+    private readonly SchoolContext _context;
+
+    public CourseRepository(SchoolContext context)
+    {
+        _context = context;
+    }
+
+    public Course GetById(long id)
+    {
+        Course course = _context.Courses.Find(id);
+        return course;
+    }
+
+    public void Save(Course course)
+    {
+        _context.Courses.Add(course);
     }
 }
